87aab92693c4f2a332e24b13059483fe2c94f3f1
Eric Bateman <bateman.eric@gmail.com>
2021-02-25 14:03:55 -0800

    Basic clean up

---
+++ app/src/main/java/com/ericbatemandev/doordashlite/DiscoverFragment.kt
@@ -0,0 +1,22 @@
+package com.ericbatemandev.doordashlite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Fragment to display a basic list of restaurants
+ */
+class DiscoverFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_discover, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
