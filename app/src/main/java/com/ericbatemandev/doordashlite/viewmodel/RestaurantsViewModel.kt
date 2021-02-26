b267a5ae4ba44a5ad072bd5aa606b976b9632cdd
Eric Bateman <bateman.eric@gmail.com>
2021-02-25 16:55:29 -0800

    Add view model

---
+++ app/src/main/java/com/ericbatemandev/doordashlite/viewmodel/RestaurantsViewModel.kt
@@ -0,0 +1,21 @@
+package com.ericbatemandev.doordashlite.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ericbatemandev.doordashlite.model.RestaurantsResponse
+import com.ericbatemandev.doordashlite.network.NetworkClient
+import com.ericbatemandev.doordashlite.network.Result
+import kotlinx.coroutines.launch
+
+class RestaurantsViewModel : ViewModel() {
+    val restaurantsResult: MutableLiveData<Result<RestaurantsResponse>> = MutableLiveData()
+
+    fun fetchRestaurantData(context: Context) {
+        viewModelScope.launch {
+            val response = NetworkClient.getRestaurants(context)
+            restaurantsResult.value = response
+        }
+    }
+}
